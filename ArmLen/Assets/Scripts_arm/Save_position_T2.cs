8e0ef862c686ee0481479f70b83e8128cc55661f
Alismo <duvan_1013@hotmail.com>
2019-11-15 12:29:23 -0500

    Subiendo (1) computacion grafica

---
+++ ArmLen/Assets/Scripts_arm/Save_position_T2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Save_position_T2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string fileName = "D:/Alismos/Documentos/Unity/BodyArm/ArmLen/Assets/Sensor2.csv";
+        float dx = transform.localRotation.eulerAngles.x;
+        float dy = transform.localRotation.eulerAngles.y;
+        float dz = transform.localRotation.eulerAngles.z;
+
+        using (StreamWriter writer = new StreamWriter(fileName, true))
+        {
+            writer.Write(transform.localPosition.x + ";" + transform.localPosition.y + ";" + transform.localPosition.z + ";" + dx + ";" + dy + ";" + dz + "\n");
+        }
+
+    }
+
+    
+}
---
+++ ArmLen/Assets/Scripts_arm/Save_position_T3.cs
@@ -0,0 +1,27 @@
+﻿
+using System.IO;
+using UnityEngine;
+
+public class Save_position_T3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string fileName = "D:/Alismos/Documentos/Unity/BodyArm/ArmLen/Assets/Sensor3.csv";
+        float dx = transform.localRotation.eulerAngles.x;
+        float dy = transform.localRotation.eulerAngles.y;
+        float dz = transform.localRotation.eulerAngles.z;
+
+        using (StreamWriter writer = new StreamWriter(fileName, true))
+        {
+            writer.Write(transform.localPosition.x + ";" + transform.localPosition.y + ";" + transform.localPosition.z + ";" + dx + ";" + dy + ";" + dz + "\n");
+        }
+
+    }
+}
